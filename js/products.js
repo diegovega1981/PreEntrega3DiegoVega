b1035b90fccf467dfa81f502efcc3b718fa28a6b
DiegoVega <diegovega1981@gmail.com>
2023-11-05 00:29:46 -0300

    actualización
    Actualización de archivos

---
+++ js/products.js
@@ -0,0 +1,66 @@
+//defino el array con los productos que se mostrarán en pantalla
+const productos = [
+    {
+        id: 1,
+        nombre: "banana x kg",
+        precio: 600,
+        img: "./images/banana.png",
+        cantidad: 1,
+    },
+
+    {
+        id: 2,
+        nombre: "pera x kg",
+        precio: 500,
+        img: "./images/pera.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 3,
+        nombre: "manzana verde x kg",
+        precio: 800,
+        img: "./images/manzana verde.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 4,
+        nombre: "manzana roja x kg",
+        precio: 700,
+        img: "./images/manzana roja.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 5,
+        nombre: "kiwi x kg",
+        precio: 1000,
+        img: "./images/kiwi.jpeg",
+        cantidad: 1,
+    },
+
+    {
+        id: 6,
+        nombre: "papa",
+        precio: 250,
+        img: "./images/papa.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 7,
+        nombre: "tomate",
+        precio: 400,
+        img: "./images/tomate.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 8,
+        nombre: "lechuga",
+        precio: 400,
+        img: "./images/lechuga.jpg",
+        cantidad: 1,
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,55 @@
+//eestas funciones permiten captar los id del html
+const shopContent = document.getElementById("ShopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+//creo un array vacío
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+productos.forEach((product) => {
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = ` 
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">$${product.precio}</p>
+    `;
+
+  shopContent.append(content);
+
+  let comprar = document.createElement("button");
+  comprar.innerText = "comprar";
+  comprar.className = "comprar";
+  content.append(comprar);
+
+  comprar.addEventListener("click", () => {
+    const repeat = carrito.some(
+      (repeatProduct) => repeatProduct.id === product.id
+    );
+    if (repeat) {
+      carrito.map((prod) => {
+        if (prod.id === product.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        id: product.id,
+        nombre: product.nombre,
+        img: product.img,
+        precio: product.precio,
+        cantidad: product.cantidad,
+      });
+    }
+    console.log(carrito);
+    carritoCounter();
+    saveLocal();
+  });
+});
+
+//set item
+const saveLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+};
+//get item
+
